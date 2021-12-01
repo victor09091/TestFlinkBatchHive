33a73a9f96cbb710f68093dde36ea9be06ca4c42
yuxiang <384669270@qq.com>
2021-12-01 14:29:32 +0800

    flink batch hive code

---
+++ src/main/scala/TestFlinkBatchHive/TestBatchHive.scala
@@ -0,0 +1,58 @@
+package TestFlinkBatchHive
+
+import org.apache.flink.table.api._
+import org.apache.flink.table.catalog.hive.HiveCatalog
+
+import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+
+/**
+ *  flink 批量读写hive
+ */
+object TestBatchHive extends App {
+  override def main(args: Array[String]): Unit = {
+
+    val settings: EnvironmentSettings = EnvironmentSettings
+      .newInstance()
+      .useBlinkPlanner()
+      .inBatchMode()
+      .build()
+
+    val tableEnv: TableEnvironment = TableEnvironment.create(settings)
+
+    // 测试flink sql  好不好用
+    val res = tableEnv.sqlQuery("select '1', '1', 1, '1', '1', '1'")
+    val r = res.execute()
+    r.print()
+
+
+    val name = "myhive" // Catalog名称，定义一个唯一的名称表示
+    val defaultDatabase = "default" // 默认数据库名称
+    val hiveConfDir = "/etc/hive/conf" // hive-site.xml路径
+    val version = "1.1.0" // Hive版本号
+
+    val hive = new HiveCatalog(name, defaultDatabase, hiveConfDir, version)
+
+    val statementSet: StatementSet = tableEnv.createStatementSet()
+    tableEnv.registerCatalog(name, hive)
+
+    tableEnv.useCatalog(name)
+
+    tableEnv.getConfig.setSqlDialect(SqlDialect.HIVE)
+    tableEnv.useDatabase("default")
+
+    // 设置读取hive的并行度
+    val configuration = tableEnv.getConfig.getConfiguration
+
+    configuration.setString("table.exec.hive.infer-source-parallelism", "false")
+    configuration.setString("table.exec.hive.infer-source-parallelism.max", "15")
+    configuration.setString("table.exec.hive.fallback-mapred-reader", "true")
+
+
+    val sqlResult = tableEnv.sqlQuery("select username, age, height, dt, hr from fs_table_source")
+
+    statementSet.addInsert("fs_table_dst", sqlResult)
+    statementSet.execute()
+
+  }
+}
+
